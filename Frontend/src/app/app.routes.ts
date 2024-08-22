2fb40607347a407925e4df783b2397e0fc247ebc
“nishidaps” <chinchoos11@gmail.com>
2024-08-22 10:13:28 +0530

    backend at

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from './common/guards/auth.guard';
 
 export const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'login', loadChildren:()=>import('./pages/pages.routes').then(x=>x.routes)},
+  {path: 'login', loadChildren:()=>import('./pages/pages.routes').then(x=>x.routes), canActivate:[AuthGuard]},
     // {
     //     path: '',
     //     loadChildren: () => import('./pages/pages.routes').then(p => p.routes)
